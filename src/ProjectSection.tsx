8e4c0477dda74546e4bd54c3d434aa39dedd4a68
Ove <osundal83@gmail.com>
2019-10-29 21:20:28 +0100

    Add basic layout

--- src/ProjectSection.tsx
+++ src/ProjectSection.tsx
@@ -1,15 +1,68 @@
 import React from "react";
 import styled from "styled-components";
 
+interface IProject {
+    title: string,
+    description: string
+}
+
+const projectList: IProject[] = [
+    {
+        title: 'project1',
+        description: 'description1'
+    },
+    {
+        title: 'project2',
+        description: 'description2'
+    },
+    {
+        title: 'project3',
+        description: 'description3'
+    },
+    {
+        title: 'project4',
+        description: 'description4'
+    }
+];
+
 export const ProjectSection: React.FC = () => {
     return (
         <ProjectSectionWrapper>
-            <p>Projects</p>
+            <ProjectList>
+                {projectList.map((project) =>
+                    <li>
+                        {Project(project)}
+                    </li>
+                )
+                }
+            </ProjectList>
         </ProjectSectionWrapper>
     );
 };
 
+function Project(project: IProject) {
+    const {title, description} = project;
+
+    return (
+        <ProjectWrapper>
+            <h1>{title}</h1>
+            <p>{description}</p>
+        </ProjectWrapper>
+    )
+}
+
 const ProjectSectionWrapper = styled.section`
     background-color: #ff7851;
     grid-column: 1 / 5;
+`;
+
+const ProjectList = styled.ul`
+  list-style: none;
+  display: flex;
+  justify-content: space-evenly;
+  align-items: center;
+`;
+
+const ProjectWrapper = styled.div`
+
 `;
